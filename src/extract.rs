f1637d4d9f5f1492b7b4a6750edbaad894acec67
0x2077 <a6b0x@outlook.com>
2025-07-04 02:07:11 +0000

    封装UniswapV2执行器

--- src/extract.rs
+++ src/extract.rs
@@ -4,20 +4,6 @@ use alloy::rpc::types::{Filter, Log, eth::Block};
 use alloy::sol;
 use eyre::Result;
 
-sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
-    UniswapV2Factory,
-    "data/UniswapV2Factory.json"
-);
-
-sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
-    UniswapV2Pair,
-    "data/UniswapV2Pair.json"
-);
-
 pub struct RpcClient {
     provider: DynProvider,
 }
@@ -46,23 +32,62 @@ impl RpcClient {
             .unwrap();
         Ok(block_data)
     }
+}
 
-    pub async fn get_uniswap_v2_all_pairs_length(&self) -> Result<u128> {
-        const UNISWAP_V2_FACTORY_ADDR: Address =
-            address!("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
-        let contract = UniswapV2Factory::new(UNISWAP_V2_FACTORY_ADDR, self.provider.clone());
-        let all_pairs_length = contract.allPairsLength().call().await?;
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    UniswapV2Router,
+    "data/UniswapV2Router02.json"
+);
+
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    UniswapV2Factory,
+    "data/UniswapV2Factory.json"
+);
+
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    UniswapV2Pair,
+    "data/UniswapV2Pair.json"
+);
+pub struct UniswapV2 {
+    provider: DynProvider,
+    router_caller: UniswapV2Router::UniswapV2RouterInstance<DynProvider>,
+    factory_caller: UniswapV2Factory::UniswapV2FactoryInstance<DynProvider>,
+}
+
+impl UniswapV2 {
+    pub async fn new(fprovider: DynProvider, router_address: Address) -> Self {
+        let router_contract = UniswapV2Router::new(router_address, fprovider.clone());
+
+        let factory_address = router_contract.factory().call().await.unwrap();
+        let factory_contract = UniswapV2Factory::new(factory_address, fprovider.clone());
+
+        Self {
+            provider: fprovider,
+            router_caller: router_contract,
+            factory_caller: factory_contract,
+        }
+    }
+
+    pub async fn get_pairs_length(&self) -> Result<u128> {
+        let all_pairs_length = self.factory_caller.allPairsLength().call().await?;
         let all_pairs_length_u128: u128 = all_pairs_length.try_into().unwrap();
         Ok(all_pairs_length_u128)
     }
 
-    pub async fn get_uniswap_v2_pair_created_events(
-        &self,
-        from_block: u64,
-        to_block: u64,
-    ) -> Result<Vec<Log>> {
-        const UNISWAP_V2_FACTORY_ADDR: Address =
-            address!("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
+    pub async fn get_pair_address(&self, pair_index_uint: Uint<256, 4>) -> Result<Address> {
+        let pair_address = self.factory_caller.allPairs(pair_index_uint).call().await?;
+        Ok(pair_address)
+    }
+
+    pub async fn get_pair_created(&self, from_block: u64, to_block: u64) -> Result<Vec<Log>> {
+        //const UNISWAP_V2_FACTORY_ADDR: Address =
+        //    address!("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
         let event_signature = keccak256(b"PairCreated(address,address,address,uint256)");
 
         let filter = Filter::new()
@@ -73,20 +98,8 @@ impl RpcClient {
         let logs = self.provider.get_logs(&filter).await?;
         Ok(logs)
     }
-
-    pub async fn get_pair_address(&self, pair_index_uint: Uint<256, 4>) -> Result<Address> {
-        const UNISWAP_V2_FACTORY_ADDR: Address =
-            address!("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
-        let factory_contract =
-            UniswapV2Factory::new(UNISWAP_V2_FACTORY_ADDR, self.provider.clone());
-        let pair_address = factory_contract
-            .allPairs(pair_index_uint)
-            .call()
-            .await?;
-        Ok(pair_address)
-    }
-
-    pub async fn get_pair_liquidity_events(
+    
+    pub async fn get_pair_liquidity(
         &self,
         pair_address: Address,
         from_block: u64,
@@ -94,7 +107,8 @@ impl RpcClient {
     ) -> Result<(Vec<Log>, Vec<Log>, Vec<Log>)> {
         let mint_event_signature = keccak256(b"Mint(address,uint256,uint256)");
         let burn_event_signature = keccak256(b"Burn(address,uint256,uint256,address)");
-        let swap_event_signature = keccak256(b"Swap(address,uint256,uint256,uint256,uint256,address)");
+        let swap_event_signature =
+            keccak256(b"Swap(address,uint256,uint256,uint256,uint256,address)");
 
         let mint_filter = Filter::new()
             .event_signature(mint_event_signature)
@@ -119,7 +133,6 @@ impl RpcClient {
         Ok((mint_logs, burn_logs, swap_logs))
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -149,30 +162,38 @@ mod tests {
             new_block_header.transactions.first_transaction()
         );
     }
+
     #[tokio::test]
-    async fn test_get_uniswap_v2_all_pairs_length() {
+    async fn test_uniswap_v2() {
         let app_config = AppConfig::new().unwrap();
         let log_level = app_config.init_log().unwrap();
         info!("app_config.log_level : {:?}", log_level);
         info!("app_config.eth: {:#?}", app_config.eth);
 
         let rpc_client = RpcClient::new(&app_config.eth.rpc_url).unwrap();
-        let res = rpc_client.get_uniswap_v2_all_pairs_length().await.unwrap();
-        info!("get_uniswap_v2_all_pairs_length: {:?}", res);
-    }
-    #[tokio::test]
-    async fn test_get_uniswap_v2_pair_created_events() {
-        let app_config = AppConfig::new().unwrap();
-        let log_level = app_config.init_log().unwrap();
-        info!("app_config.log_level : {:?}", log_level);
-        info!("app_config.eth: {:#?}", app_config.eth);
 
-        let rpc_client = RpcClient::new(&app_config.eth.rpc_url).unwrap();
+        let router_addr = address!("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+        let uniswap_v2 = UniswapV2::new(rpc_client.provider.clone(), router_addr).await;
+        info!(
+            "uniswap_v2 factory_caller: {:#?}",
+            uniswap_v2.factory_caller
+        );
+
+        let pair_length = uniswap_v2.get_pairs_length().await.unwrap();
+        info!("get_pairs_length: {:?}", pair_length);
+
+        let latest_pair_index = pair_length.saturating_sub(1);
+        let latest_pair_index_uint: Uint<256, 4> = Uint::from(latest_pair_index);
+        let latest_pair_address = uniswap_v2
+            .get_pair_address(latest_pair_index_uint)
+            .await
+            .unwrap();
+        info!("get_pair_address(latest): {:?}", latest_pair_address);
 
         let from_block = 22770510;
         let to_block = 22770512;
-        let events = rpc_client
-            .get_uniswap_v2_pair_created_events(from_block, to_block)
+        let pair_created_events = uniswap_v2
+            .get_pair_created(from_block, to_block)
             .await
             .unwrap();
 
@@ -180,53 +201,23 @@ mod tests {
             "Number of PairCreated events in block range {} - {}: {}",
             from_block,
             to_block,
-            events.len()
+            pair_created_events.len()
         );
         //info!("test_get_uniswap_v2_pair_created_events: {:#?}", events.first());
-        info!("test_get_uniswap_v2_pair_created_events: {:#?}", events);
-    }
-    #[tokio::test]
-    async fn test_get_latest_pair_address() {
-        let app_config = AppConfig::new().unwrap();
-        let log_level = app_config.init_log().unwrap();
-        info!("app_config.log_level : {:?}", log_level);
-        info!("app_config.eth: {:#?}", app_config.eth);
+        info!("get_pair_created: {:#?}", pair_created_events);
 
-        let rpc_client = RpcClient::new(&app_config.eth.rpc_url).unwrap();
+        let pair_address = address!("0xaAF2fe003BB967EB7C35A391A2401e966bdB7F95");
+        let from_block1 = 22828657;
+        let to_block1 = 22828661;
 
-        let all_pairs_length = rpc_client.get_uniswap_v2_all_pairs_length().await.unwrap();
-        info!("get_uniswap_v2_all_pairs_length: {:?}", all_pairs_length);
-
-        let latest_pair_index = all_pairs_length.saturating_sub(1);
-        let latest_pair_index_uint: Uint<256, 4> = Uint::from(latest_pair_index);
-
-        let latest_pair_address = rpc_client
-            .get_pair_address(latest_pair_index_uint)
+        let (mint_logs, burn_logs, swap_logs) = uniswap_v2
+            .get_pair_liquidity(pair_address, from_block1, to_block1)
             .await
             .unwrap();
-        info!("Latest Uniswap V2 pair address: {:?}", latest_pair_address);
-    }
-
-    #[tokio::test]
-    async fn test_get_latest_pair_liquidity_events() {
-        let app_config = AppConfig::new().unwrap();
-        let log_level = app_config.init_log().unwrap();
-        info!("app_config.log_level : {:?}", log_level);
-        info!("app_config.eth: {:#?}", app_config.eth);
-
-        let rpc_client = RpcClient::new(&app_config.eth.rpc_url).unwrap();
-
-        let pair_address_str = "0xaAF2fe003BB967EB7C35A391A2401e966bdB7F95";
-        let from_block = 22828657;
-        let to_block = 22828661;
-
-        let pair_address = pair_address_str.parse().unwrap();
-        let (mint_logs, burn_logs, swap_logs) = rpc_client
-            .get_pair_liquidity_events(pair_address, from_block, to_block)
-            .await
-            .unwrap();
-        info!("mint_logs: {:#?}", mint_logs);
-        info!("burn_logs: {:#?}", burn_logs);
-        info!("swap_logs: {:#?}", swap_logs);
+        info!(
+            "get_pair_liquidity pair_address:{} mint_logs: {:#?} burn_logs: {:#?} swap_logs: {:#?}",
+            pair_address,
+            mint_logs, burn_logs, swap_logs
+        );
     }
 }
