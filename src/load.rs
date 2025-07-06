ce7d04ff207ae89caf271a4bd30892b8f9862969
0x2077 <a6b0x@outlook.com>
2025-07-06 04:53:48 +0000

    存储UniswapV2事件

--- src/load.rs
+++ src/load.rs
@@ -12,19 +12,47 @@ pub struct TableBlock {
     pub miner: String,
     pub date_time: String,
 }
+#[derive(Debug, Serialize)]
+pub struct UniswapV2Event {
+    pub block_time: String,
+    pub block_number: u64,
+    pub transaction_hash: String,
+    pub event_type: String,
+    pub from_address: String,
+    pub to_address: String,
+    pub token0: String,
+    pub token1: String,
+    pub amount0: String,
+    pub amount1: String,
+}
 
 impl CsvFile {
     pub fn new(filename: &str) -> Result<Self> {
-        let file = File::create(filename).context("Failed to create file")?;
+        //let file = File::create(filename).context("Failed to create file")?;
+        let file = File::options()
+            .create(true)
+            .append(true)
+            .open(filename)
+            .context("Failed to open file")?;
         let mut writer = Writer::from_writer(file);
         //writer.write_record(&["block_number","tx_count","miner","date_time"])
         //    .context("Failed to write record header")?;
         Ok(Self { writer })
     }
+
     pub fn write_block(&mut self, block: &TableBlock) -> Result<()> {
         self.writer
             .serialize(block)
             .context("Failed to write block data")
+    }
+
+    pub fn write_event(&mut self, events: &[UniswapV2Event]) -> Result<()> {
+        for event in events {
+            self.writer
+                .serialize(event)
+                .context("Failed to write event data")?;
+        }
+        Ok(())
     }
 }
 
@@ -32,7 +60,12 @@ impl CsvFile {
 
 mod tests {
     use super::*;
-    use crate::{extract::RpcClient, init::AppConfig, transform::transform_block};
+    use crate::{
+        extract::RpcClient, extract::UniswapV2, init::AppConfig, transform::transform_block,
+        transform::transform_burn_event, transform::transform_mint_event,
+        transform::transform_pair_created_event, transform::transform_swap_event,
+    };
+    use alloy::primitives::address;
     use eyre::{Ok, Result};
     use log::info;
     use std::io::Write;
@@ -82,5 +115,121 @@ mod tests {
             };
             csv_file.write_block(&block).unwrap();
         }
+    }
+
+    #[tokio::test]
+    async fn test_load_event() {
+        let app_config = AppConfig::new().unwrap();
+        let log_level = app_config.init_log().unwrap();
+        info!("app_config.log_level: {:?}", log_level);
+
+        let rpc_client = RpcClient::new(&app_config.eth.rpc_url).unwrap();
+        let router_addr = address!("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+        let uniswap_v2 = UniswapV2::new(rpc_client.provider.clone(), router_addr).await;
+        info!(
+            "uniswap_v2 factory_caller: {:#?}",
+            uniswap_v2.factory_caller
+        );
+
+        let from_block = 22828657;
+        let to_block = 22828661;
+        let pair_created_events = uniswap_v2
+            .get_pair_created(from_block, to_block)
+            .await
+            .unwrap();
+        info!("pair_created_events: {:#?}", pair_created_events);
+
+        let pair_created_event = transform_pair_created_event(&pair_created_events).unwrap();
+        info!("pair_created_event: {:#?}", pair_created_event);
+
+        let mut csv_file = CsvFile::new("data/event.csv").unwrap();
+        let pair_created_events = pair_created_event
+            .iter()
+            .map(|event| UniswapV2Event {
+                block_time: event.date_time.to_string(),
+                block_number: event.block_number,
+                transaction_hash: event.tx_hash.clone(),
+                event_type: "PairCreated".to_string(),
+                from_address: event.factory_address.to_string(),
+                to_address: event.pair_address.to_string(),
+                token0: event.token0.to_string(),
+                token1: event.token1.to_string(),
+                amount0: 0.to_string(),
+                amount1: 0.to_string(),
+            })
+            .collect::<Vec<_>>();
+        csv_file.write_event(&pair_created_events).unwrap();
+
+        let pair_address = address!("0xaAF2fe003BB967EB7C35A391A2401e966bdB7F95");
+        let (mint_logs, burn_logs, swap_logs) = uniswap_v2
+            .get_pair_liquidity(pair_address, from_block, to_block)
+            .await
+            .unwrap();
+        info!(
+            "get_pair_liquidity pair_address: {} 
+            mint_logs: {:#?} burn_logs: {:#?} swap_logs: {:#?}",
+            pair_address, mint_logs, burn_logs, swap_logs
+        );
+
+        let mint_events = transform_mint_event(&mint_logs).unwrap();
+        info!("mint_events: {:#?}", mint_events);
+        let mint_uniswap_events = mint_events
+            .iter()
+            .map(|event| UniswapV2Event {
+                block_time: event.date_time.to_string(),
+                block_number: event.block_number,
+                transaction_hash: event.tx_hash.clone(),
+                event_type: "Mint".to_string(),
+                from_address: event.sender.to_string(),
+                to_address:pair_address.to_string(),
+                token0: "null".to_string(),
+                token1: "null".to_string(),
+                amount0: event.amount0.to_string(),
+                amount1: event.amount1.to_string(),
+            })
+            .collect::<Vec<_>>();
+        csv_file.write_event(&mint_uniswap_events).unwrap();
+
+        let burn_events = transform_burn_event(&burn_logs).unwrap();
+        info!("burn_events: {:#?}", burn_events);
+        let burn_uniswap_events = burn_events
+            .iter()
+            .map(|event| UniswapV2Event {
+                block_time: event.date_time.to_string(),
+                block_number: event.block_number,
+                transaction_hash: event.tx_hash.clone(),
+                event_type: "Burn".to_string(),
+                from_address: event.sender.to_string(),
+                to_address: event.address.to_string(),
+                token0: "null".to_string(),
+                token1: "null".to_string(),
+                amount0: event.amount0.to_string(),
+                amount1: event.amount1.to_string(),
+            })
+            .collect::<Vec<_>>();
+        csv_file.write_event(&burn_uniswap_events).unwrap();
+
+        let swap_events = transform_swap_event(&swap_logs).unwrap();
+        info!("swap_events: {:#?}", swap_events);
+        let swap_uniswap_events = swap_events
+            .iter()
+            .map(|event| {
+                let total_amount0 = event.amount0_in + event.amount0_out;
+                let total_amount1 = event.amount1_in + event.amount1_out;
+                UniswapV2Event {
+                    block_time: event.date_time.to_string(),
+                    block_number: event.block_number,
+                    transaction_hash: event.tx_hash.clone(),
+                    event_type: "Swap".to_string(),
+                    from_address: event.sender.to_string(),
+                    to_address: event.address.to_string(),
+                    token0: "null".to_string(),
+                    token1: "null".to_string(),
+                    amount0: total_amount0.to_string(),
+                    amount1: total_amount1.to_string(),
+                }
+            })
+            .collect::<Vec<_>>();
+        csv_file.write_event(&swap_uniswap_events).unwrap();
     }
 }
--- src/transform.rs
+++ src/transform.rs
@@ -34,23 +34,40 @@ pub struct PairCreatedEvent {
     pub token1: Address,
     pub block_number: u64,
     pub tx_hash: String,
+    pub factory_address: Address,
+    pub pair_address: Address,
     pub date_time: DateTime<Local>,
 }
 
-pub fn transform_pair_created_event(log: &Log) -> Result<PairCreatedEvent> {
-    if log.topics().len() < 3 {
-        return Err(eyre::eyre!("Invalid PairCreated event log topics length"));
-    }
-    let fun_signature = log.topics()[0].to_string();
-    let token0 = Address::from_slice(&log.topics()[1][12..32]);
-    let token1 = Address::from_slice(&log.topics()[2][12..32]);
-    let block_number = log.block_number.unwrap();
-    let tx_hash = log.transaction_hash.unwrap().to_string();
-    let date_time = DateTime::<Utc>::from_timestamp(log.block_timestamp.unwrap() as i64, 0)
-        .context("Failed to convert timestamp to DateTime")?;
-    let local_date_time = date_time.with_timezone(&Local);
+pub fn transform_pair_created_event(logs: &[Log]) -> Result<Vec<PairCreatedEvent>> {
+    let mut events = Vec::new();
+    for log in logs {
+        if log.topics().len() < 3 {
+            return Err(eyre::eyre!("Invalid PairCreated event log topics length"));
+        }
+        let fun_signature = log.topics()[0].to_string();
+        let token0 = Address::from_slice(&log.topics()[1][12..32]);
+        let token1 = Address::from_slice(&log.topics()[2][12..32]);
+        let block_number = log.block_number.unwrap();
+        let tx_hash = log.transaction_hash.unwrap().to_string();
+        let factory_address = log.address();
+        let pair_address = Address::from_slice(&log.data().data[12..32]);
+        let date_time = DateTime::<Utc>::from_timestamp(log.block_timestamp.unwrap() as i64, 0)
+            .context("Failed to convert timestamp to DateTime")?;
+        let local_date_time = date_time.with_timezone(&Local);
 
-    Ok(PairCreatedEvent { fun_signature, token0, token1, block_number, tx_hash, date_time: local_date_time })
+        events.push(PairCreatedEvent {
+            fun_signature,
+            token0,
+            token1,
+            block_number,
+            tx_hash,
+            factory_address,
+            pair_address,
+            date_time: local_date_time,
+        });
+    }
+    Ok(events)
 }
 
 #[derive(Debug)]
@@ -64,35 +81,41 @@ pub struct MintEvent {
     pub date_time: DateTime<Local>,
 }
 
-pub fn transform_mint_event(log: &Log) -> Result<MintEvent> {
-    if log.topics().len() < 2 {
-        return Err(eyre::eyre!("Invalid Mint event log topics length"));
+pub fn transform_mint_event(logs: &[Log]) -> Result<Vec<MintEvent>> {
+    let mut events = Vec::new();
+    for log in logs {
+        if log.topics().len() < 2 {
+            return Err(eyre::eyre!("Invalid Mint event log topics length"));
+        }
+        let fun_signature = log.topics()[0].to_string();
+        let sender = Address::from_slice(&log.topics()[1][12..32]);
+
+        let log_data = log.data().data.clone();
+        if log_data.len() < 48 {
+            return Err(eyre::eyre!(
+                "Mint event log data length is less than 48 bytes"
+            ));
+        }
+        let amount0 = u128::from_be_bytes(log_data[16..32].try_into().unwrap());
+        let amount1 = u128::from_be_bytes(log_data[48..64].try_into().unwrap());
+
+        let block_number = log.block_number.unwrap();
+        let tx_hash = log.transaction_hash.unwrap().to_string();
+        let date_time = DateTime::<Utc>::from_timestamp(log.block_timestamp.unwrap() as i64, 0)
+            .context("Failed to convert timestamp to DateTime")?;
+        let local_date_time = date_time.with_timezone(&Local);
+
+        events.push(MintEvent {
+            fun_signature,
+            sender,
+            amount0,
+            amount1,
+            block_number,
+            tx_hash,
+            date_time: local_date_time,
+        });
     }
-    let fun_signature = log.topics()[0].to_string();
-    let sender = Address::from_slice(&log.topics()[1][12..32]);
-
-    let log_data= log.data().data.clone();
-    if log_data.len() < 48 {
-        return Err(eyre::eyre!("Mint event log data length is less than 48 bytes"));
-    }
-    let amount0 = u128::from_be_bytes(log_data[16..32].try_into().unwrap());
-    let amount1 = u128::from_be_bytes(log_data[48..64].try_into().unwrap());
-
-    let block_number = log.block_number.unwrap();
-    let tx_hash = log.transaction_hash.unwrap().to_string();
-    let date_time = DateTime::<Utc>::from_timestamp(log.block_timestamp.unwrap() as i64, 0)
-        .context("Failed to convert timestamp to DateTime")?;
-    let local_date_time = date_time.with_timezone(&Local);
-
-    Ok(MintEvent {
-        fun_signature,
-        sender,
-        amount0,
-        amount1,
-        block_number,
-        tx_hash,
-        date_time: local_date_time,
-    })
+    Ok(events)
 }
 
 #[derive(Debug)]
@@ -107,37 +130,43 @@ pub struct BurnEvent {
     pub date_time: DateTime<Local>,
 }
 
-pub fn transform_burn_event(log: &Log) -> Result<BurnEvent> {
-    if log.topics().len() < 3 {
-        return Err(eyre::eyre!("Invalid Burn event log topics length"));
+pub fn transform_burn_event(logs: &[Log]) -> Result<Vec<BurnEvent>> {
+    let mut events = Vec::new();
+    for log in logs {
+        if log.topics().len() < 3 {
+            return Err(eyre::eyre!("Invalid Burn event log topics length"));
+        }
+        let fun_signature = log.topics()[0].to_string();
+        let sender = Address::from_slice(&log.topics()[1][12..32]);
+        let address = Address::from_slice(&log.topics()[2][12..32]);
+
+        let log_data = log.data().data.clone();
+        if log_data.len() < 64 {
+            return Err(eyre::eyre!(
+                "Burn event log data length is less than 64 bytes"
+            ));
+        }
+        let amount0 = u128::from_be_bytes(log_data[16..32].try_into().unwrap());
+        let amount1 = u128::from_be_bytes(log_data[48..64].try_into().unwrap());
+
+        let block_number = log.block_number.unwrap();
+        let tx_hash = log.transaction_hash.unwrap().to_string();
+        let date_time = DateTime::<Utc>::from_timestamp(log.block_timestamp.unwrap() as i64, 0)
+            .context("Failed to convert timestamp to DateTime")?;
+        let local_date_time = date_time.with_timezone(&Local);
+
+        events.push(BurnEvent {
+            fun_signature,
+            sender,
+            address,
+            amount0,
+            amount1,
+            block_number,
+            tx_hash,
+            date_time: local_date_time,
+        });
     }
-    let fun_signature = log.topics()[0].to_string();
-    let sender = Address::from_slice(&log.topics()[1][12..32]);
-    let address = Address::from_slice(&log.topics()[2][12..32]);
-
-    let log_data = log.data().data.clone(); 
-    if log_data.len() < 64 {
-        return Err(eyre::eyre!("Burn event log data length is less than 64 bytes"));
-    }
-    let amount0 = u128::from_be_bytes(log_data[16..32].try_into().unwrap());
-    let amount1 = u128::from_be_bytes(log_data[48..64].try_into().unwrap());
-
-    let block_number = log.block_number.unwrap();
-    let tx_hash = log.transaction_hash.unwrap().to_string();
-    let date_time = DateTime::<Utc>::from_timestamp(log.block_timestamp.unwrap() as i64, 0)
-        .context("Failed to convert timestamp to DateTime")?;
-    let local_date_time = date_time.with_timezone(&Local);
-
-    Ok(BurnEvent {
-        fun_signature,
-        sender,
-        address,
-        amount0,
-        amount1,
-        block_number,
-        tx_hash,
-        date_time: local_date_time,
-    })
+    Ok(events)
 }
 
 #[derive(Debug)]
@@ -154,42 +183,47 @@ pub struct SwapEvent {
     pub date_time: DateTime<Local>,
 }
 
-pub fn transform_swap_event(log: &Log) -> Result<SwapEvent> {
-    if log.topics().len() < 3 {
-        return Err(eyre::eyre!("Invalid Swap event log topics length"));
-    }
-    let fun_signature = log.topics()[0].to_string();
-    let sender = Address::from_slice(&log.topics()[1][12..32]);
-    let address = Address::from_slice(&log.topics()[2][12..32]);
-    
+pub fn transform_swap_event(logs: &[Log]) -> Result<Vec<SwapEvent>> {
+    let mut events = Vec::new();
+    for log in logs {
+        if log.topics().len() < 3 {
+            return Err(eyre::eyre!("Invalid Swap event log topics length"));
+        }
+        let fun_signature = log.topics()[0].to_string();
+        let sender = Address::from_slice(&log.topics()[1][12..32]);
+        let address = Address::from_slice(&log.topics()[2][12..32]);
 
-    let log_data = log.data().data.clone(); 
-    if log_data.len() < 128 {
-        return Err(eyre::eyre!("Swap event log data length is less than 64 bytes"));
-    }
-    let amount0_in = u128::from_be_bytes(log_data[16..32].try_into().unwrap());
-    let amount1_in = u128::from_be_bytes(log_data[48..64].try_into().unwrap());
-    let amount0_out = u128::from_be_bytes(log_data[80..96].try_into().unwrap());
-    let amount1_out = u128::from_be_bytes(log_data[112..128].try_into().unwrap());
-    
-    let block_number = log.block_number.unwrap();
-    let tx_hash = log.transaction_hash.unwrap().to_string();
-    let date_time = DateTime::<Utc>::from_timestamp(log.block_timestamp.unwrap() as i64, 0)
-        .context("Failed to convert timestamp to DateTime")?;
-    let local_date_time = date_time.with_timezone(&Local);
+        let log_data = log.data().data.clone();
+        if log_data.len() < 128 {
+            return Err(eyre::eyre!(
+                "Swap event log data length is less than 128 bytes"
+            ));
+        }
+        let amount0_in = u128::from_be_bytes(log_data[16..32].try_into().unwrap());
+        let amount1_in = u128::from_be_bytes(log_data[48..64].try_into().unwrap());
+        let amount0_out = u128::from_be_bytes(log_data[80..96].try_into().unwrap());
+        let amount1_out = u128::from_be_bytes(log_data[112..128].try_into().unwrap());
 
-    Ok(SwapEvent {
-        fun_signature,
-        sender,
-        address,
-        amount0_in,
-        amount1_in,
-        amount0_out,
-        amount1_out,
-        block_number,
-        tx_hash,
-        date_time: local_date_time,
-    })
+        let block_number = log.block_number.unwrap();
+        let tx_hash = log.transaction_hash.unwrap().to_string();
+        let date_time = DateTime::<Utc>::from_timestamp(log.block_timestamp.unwrap() as i64, 0)
+            .context("Failed to convert timestamp to DateTime")?;
+        let local_date_time = date_time.with_timezone(&Local);
+
+        events.push(SwapEvent {
+            fun_signature,
+            sender,
+            address,
+            amount0_in,
+            amount1_in,
+            amount0_out,
+            amount1_out,
+            block_number,
+            tx_hash,
+            date_time: local_date_time,
+        });
+    }
+    Ok(events)
 }
 
 #[cfg(test)]
@@ -248,12 +282,8 @@ mod tests {
             .await
             .unwrap();
         info!("pair_created_events: {:#?}", pair_created_events);
-        if let Some(event) = pair_created_events.first() {
-            info!("First event topics length: {}", event.topics().len());
-            info!("First event topics: {:#?}", event.topics());
-            let pair_created_event = transform_pair_created_event(event).unwrap();
-            info!("pair_created_event: {:#?}", pair_created_event);
-        }
+        let transformed_events = transform_pair_created_event(&pair_created_events).unwrap();
+        info!("transformed_events: {:#?}", transformed_events);
     }
 
     #[tokio::test]
@@ -280,19 +310,16 @@ mod tests {
         info!(
             "get_pair_liquidity pair_address: {} 
             mint_logs: {:#?} burn_logs: {:#?} swap_logs: {:#?}",
-            pair_address,
-            mint_logs, burn_logs, swap_logs
+            pair_address, mint_logs, burn_logs, swap_logs
         );
 
-        let mint_event = transform_mint_event(mint_logs.first().unwrap()).unwrap();
-        info!("mint_event: {:#?}", mint_event);
+        let mint_events = transform_mint_event(&mint_logs).unwrap();
+        info!("mint_events: {:#?}", mint_events);
 
-        let burn_event = transform_burn_event(burn_logs.first().unwrap()).unwrap();
-        info!("burn_event: {:#?}", burn_event);
+        let burn_events = transform_burn_event(&burn_logs).unwrap();
+        info!("burn_events: {:#?}", burn_events);
 
-        let swap_event = transform_swap_event(swap_logs.first().unwrap()).unwrap();
-        info!("swap_event: {:#?}", swap_event);
-
+        let swap_events = transform_swap_event(&swap_logs).unwrap();
+        info!("swap_events: {:#?}", swap_events);
     }
-
 }
